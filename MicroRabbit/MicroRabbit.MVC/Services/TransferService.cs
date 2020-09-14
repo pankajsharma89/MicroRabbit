05568274bf602304f8197781e5acf4d4d0999566
Pankaj Sharma <sharmapankaj133@gmail.com>
2020-09-14 09:48:35 +0530

    .Net Core Microservice with RabbitMQ

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,37 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto),
+                                            System.Text.Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await _apiClient.PostAsync(uri, transferContent);
+                response.EnsureSuccessStatusCode();
+            }catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
